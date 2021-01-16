287bee87363d71ea5ce6e06538a1efd8a9f2403d
Adist319 <59300795+Adist319@users.noreply.github.com>
2021-01-16 12:21:14 -0800

    Update main.py

--- HelloVorl/main.py
+++ HelloVorl/main.py
@@ -1,4 +1,4 @@
 age = int(input("How old are you?"))
 name = input("What is your name?")
-print(f"My name is {name}, and I am {} years old.")
+print(f"My name is {name}, and I am {age} years old.")
 
