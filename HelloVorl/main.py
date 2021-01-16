09f59adbc6aed25b5eb751034af4eb943c645769
Adist319 <59300795+Adist319@users.noreply.github.com>
2021-01-16 13:32:47 -0800

    Update main.py
    Test commit

--- HelloVorl/main.py
+++ HelloVorl/main.py
@@ -1,4 +1,4 @@
 age = int(input("How old are you?"))
 name = input("What is your name?")
-print(f"My name is {name}, and I am {age} years old.")
-
+school = input("What school do you go to?)
+print(f"My name is {name}, and I am {age} years old. I go to the {school}.")
