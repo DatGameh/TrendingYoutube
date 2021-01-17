c3a95d6b77ad67f3687e9bfe2a59c6900457740b
Adist319 <59300795+Adist319@users.noreply.github.com>
2021-01-16 17:03:01 -0800

    Update test.py
    Created a table to display specific fields about the videos.
    TO-DO: filter them out based on the given start and end dates. 
    TO-DO if time permits: sort the filtered videos by the given metrics (views, likes, dislikes, comment_count).

--- HelloVorl/test.py
+++ HelloVorl/test.py
@@ -4,6 +4,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import datetime
+import os
 
 st.title("Test Project")
 df = pd.DataFrame({
@@ -25,6 +26,28 @@ if start_date < end_date:
 else:
     st.error('Error: End date must fall after start date.')
 
-# TODO: include the youtube statistics in the US later
-# TODO: filter out the results using start date and end date
-# TODO: displays view count, likes, dislikes, video name, channel name, number of comments (if possible) and URL's to the videos if possible
+# TODO: include the .json and .csv of YouTube statistics for the US (DONE)
+for dirname, _, filenames in os.walk('C:/Users/ongad/OneDrive/Desktop/CruzHacks Data'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+nRowsRead = None  # specify 'None' if want to read whole file
+# CAvideos.csv has 40881 rows in reality, but we are only loading/previewing the first 1000 rows
+df1 = pd.read_csv('C:/Users/ongad/OneDrive/Desktop/CruzHacks Data/USvideos.csv', delimiter=',', nrows=nRowsRead)
+df1.dataframeName = 'USvideos.csv'
+nRow, nCol = df1.shape
+
+# define variables for video link, title, views, likes, dislikes, comments, date columns
+link = df1.video_id
+title = df1.title
+views = df1.views
+likes = df1.likes
+dislikes = df1.dislikes
+comments = df1.comment_count
+dates = df1.trending_date
+print(f'There are {nRow} rows and {nCol} columns')
+df1.head(5)
+# TODO: Once the table is set up properly, with the results filtered by start and end date, we can include a SelectBox
+#  to display certain metrics like the ones we defined above sorted in descending order.
+
+DATA_URL = ('USvideos.csv')
+st.dataframe({"Link": link, "Trending Date": dates, "Title": title, "Views": views, "Likes": likes, "Dislikes": dislikes, "Comment count": comments})
